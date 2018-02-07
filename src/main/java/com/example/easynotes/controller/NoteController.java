54a424bae2940a717eaa054993069ed861c25b99
Matt Bednarski <bednarskimat@Matts-MacBook-Pro.local>
2018-02-07 08:10:57 -0500

    initial commit

---
+++ src/main/java/com/example/easynotes/controller/NoteController.java
@@ -0,0 +1,69 @@
+package com.example.easynotes.controller;
+
+import java.util.*;
+import com.example.easynotes.model.Note;
+import com.example.easynotes.repository.NotesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.xml.ws.Response;
+
+@RestController
+@RequestMapping("/api")
+public class NoteController {
+
+    @Autowired
+    NotesRepository noteRepository;
+    // get all notes
+
+    @GetMapping("/notes")
+    public List<Note> getAllNotes(){
+        return noteRepository.findAll();
+    }
+
+    // create a new note
+    @PostMapping("/notes")
+    public Note createNote(@Valid @RequestBody Note note) {
+        return noteRepository.save(note);
+    }
+    //get a single note
+
+    @GetMapping("/notes/{id}")
+    public ResponseEntity<Note> getNoteById(@PathVariable(value = "id") Long noteId){
+        Note note = noteRepository.findOne(noteId);
+        if(note == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().body(note);
+    }
+
+    //update a note
+    @PutMapping("/notes/{id}")
+    public ResponseEntity<Note> updateNote(@PathVariable(value = "id") Long noteId, @Valid @RequestBody Note noteDetails){
+        Note note = noteRepository.findOne(noteId);
+
+        if(note == null ){
+            return ResponseEntity.notFound().build();
+        }
+
+        note.setTitle(noteDetails.getTitle());
+        note.setContent(noteDetails.getContent());
+
+        Note updatedNote = noteRepository.save(note);
+        return ResponseEntity.ok(updatedNote);
+    }
+
+    //delete a note
+    @DeleteMapping("/notes/{id}")
+    public ResponseEntity<Note> deleteNote(@PathVariable(value = "id") Long noteId) {
+        Note note = noteRepository.findOne(noteId);
+        if(note == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        noteRepository.delete(note);
+        return ResponseEntity.ok().build();
+    }
+}
